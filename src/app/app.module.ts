f0d904a8e7efce4781da33df924009acdb50fdc2
Om Prakash Vangara <omee1188@gmail.com>
2025-04-20 07:34:25 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     RouterModule,
     AppRoutingModule
   ],
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,28 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup; // Add the non-null assertion operator
+  submitted = false;
 
-export class ContactComponent {
-  name: string;
-  email: string;
-  message: string;
+  constructor(private fb: FormBuilder) {
+    // Initialize the form in constructor
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
 
-  constructor() {
-    this.name = '';
-    this.email = '';
-    this.message = '';
+  ngOnInit() {
+    // Remove form initialization from ngOnInit since it's now in constructor
   }
 
   onSubmit() {
-    // Handle form submission logic here
-    console.log('Form submitted:', {
-      name: this.name,
-      email: this.email,
-      message: this.message,
-    });
+    this.submitted = true;
+    if (this.contactForm.valid) {
+      console.log('Form submitted:', this.contactForm.value);
+      this.contactForm.reset();
+      this.submitted = false;
+    }
   }
+
+  get f() { return this.contactForm.controls; }
 }
